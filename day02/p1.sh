073cad4b475f0743503a8c67a80dee1639143992
Matthew Maycock <ummaycoc@gmail.com>
2022-12-02 14:15:35 -0500

    Update p1.sh
    Decided to drop first `bc` despite the fact that it felt better mental model wise -- this is fine given PEMDAS.

--- day02/p1.sh
+++ day02/p1.sh
@@ -1,4 +1,4 @@
-choices=$( < input cut -d' ' -f2 | sort | uniq -c | rev | cut -d' ' -f2 | rev | nl -w 1 | sed -e 's/\t/ * /' | bc | paste -d+ - - - | bc )
+choices=$( < input cut -d' ' -f2 | sort | uniq -c | rev | cut -d' ' -f2 | rev | nl -w 1 | sed -e 's/\t/*/' | paste -d+ - - - | bc )
 draws=$( < input egrep '(A X|B Y|C Z)' | wc -l )
 wins=$( < input egrep '(A Y|B Z|C X)' | wc -l )
 echo $(( choices + 3 * draws + 6 * wins ))
