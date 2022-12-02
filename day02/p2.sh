48cd99d7a8c70c077d18190aa30f3610f0d75eac
Matthew Maycock <ummaycoc@gmail.com>
2022-12-02 00:23:10 -0500

    Create p2.sh

---
+++ day02/p2.sh
@@ -0,0 +1,5 @@
+results=$( < input cut -d' ' -f2 | sort | uniq -c | rev | cut -d' ' -f2 | rev | nl -w1 -v0 | sed -e 's/\t/ * 3 * /' | bc | paste -d+ - - - | bc )
+rocks=$( < input egrep '(A Y|B X|C Z)' | wc -l )
+papers=$( < input egrep '(B Y|C X|A Z)' | wc -l )
+scissors=$( < input egrep '(C Y|A X|B Z)' | wc -l )
+echo $(( results + 1 * rocks + 2 * papers + 3 * scissors ))
