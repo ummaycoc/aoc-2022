6491a3c2392a6adfd5f8f76d6da1d22f0ab61c10
Matthew Maycock <ummaycoc@gmail.com>
2022-12-02 14:15:57 -0500

    Update p2.sh
    Decided to drop first `bc` despite the fact that it felt better mental model wise -- this is fine given PEMDAS.

--- day02/p2.sh
+++ day02/p2.sh
@@ -1,4 +1,4 @@
-results=$( < input cut -d' ' -f2 | sort | uniq -c | rev | cut -d' ' -f2 | rev | nl -w1 -v0 | sed -e 's/\t/ * 3 * /' | bc | paste -d+ - - - | bc )
+results=$( < input cut -d' ' -f2 | sort | uniq -c | rev | cut -d' ' -f2 | rev | nl -w1 -v0 | sed -e 's/\t/*3*/' | paste -d+ - - - | bc )
 rocks=$( < input egrep '(A Y|B X|C Z)' | wc -l )
 papers=$( < input egrep '(B Y|C X|A Z)' | wc -l )
 scissors=$( < input egrep '(C Y|A X|B Z)' | wc -l )
