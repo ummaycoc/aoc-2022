8797285b89cffe57433b5c4d6f320097a7362944
Matthew Maycock <ummaycoc@gmail.com>
2022-12-04 00:46:26 -0500

    Create p2.sh

---
+++ day04/p2.sh
@@ -0,0 +1,11 @@
+while read ; do
+  nums=( $( <<< "$REPLY" tr ',-' ' ' ) )
+  if (
+    ([ ${nums[0]} -le ${nums[2]} ] && [ ${nums[1]} -ge ${nums[3]} ]) ||
+    ([ ${nums[0]} -ge ${nums[2]} ] && [ ${nums[1]} -le ${nums[3]} ]) ||
+    ([ ${nums[0]} -le ${nums[2]} ] && [ ${nums[2]} -le ${nums[1]} ]) ||
+    ([ ${nums[0]} -le ${nums[3]} ] && [ ${nums[3]} -le ${nums[1]} ])
+  ) ; then
+    echo 1
+  fi
+done < input | paste -s -d+ - | bc
