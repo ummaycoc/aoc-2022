15c05361ef0fc708efc6d48318ebe4bb78e2efa9
Matthew Maycock <ummaycoc@gmail.com>
2022-12-08 20:00:56 -0500

    Create p1.sh

---
+++ day07/p1.sh
@@ -0,0 +1,70 @@
+function readinput {
+  DIR=
+  while read ; do
+    case "$REPLY" in
+    '$ cd /')
+      DIR=/
+      ;;
+    '$ cd ..')
+      DIR="$( dirname "$DIR" )"
+      ;;
+    '$ cd '*)
+      if [ "$DIR" = / ] ; then
+        DIR="/${REPLY:5}"
+      else
+        DIR="${DIR}/${REPLY:5}"
+      fi
+      ;;
+    '$ ls')
+      ;; # ignore
+    'dir '*)
+      ;; # ignore
+    *)
+      SIZE="$( <<< "$REPLY" cut -d' ' -f1 )"
+      echo "$( <<< "${DIR}/" sed -e 's!//!/!' | grep -o / | tr -d $'\n' ) $DIR $SIZE"
+      ;;
+    esac
+  done < input
+}
+
+function combine {
+  PREV=
+  while read ; do
+    if [ -z "$PREV" ] ; then
+      PREV="$REPLY"
+      continue
+    fi
+    PENTRY=( $PREV )
+    CENTRY=( $REPLY )
+    if [ "${PENTRY[1]}" = "${CENTRY[1]}" ] ; then
+      PREV="${PENTRY[0]} ${PENTRY[1]} $(( PENTRY[2] + CENTRY[2] ))"
+    else
+      echo "$PREV"
+      PREV="$REPLY"
+    fi
+  done
+  echo "$PREV"
+}
+
+function process {
+  LINES="$( sort | tail -r | combine )"
+  while [ $( <<< "$LINES" wc -l ) -gt 1 ] ; do
+    PREFIX=$( <<< "$LINES" head -n 1 | cut -d' ' -f1 )
+    <<< "$LINES" grep "^${PREFIX}" | cut -d' ' -f2-
+    PRUNED=$( <<< "$LINES" grep "^${PREFIX}" | prune )
+    UNPRUNED=$( <<< "$LINES" grep -v "^${PREFIX}" )
+    LINES=$( printf '%s\n' "$PRUNED" "$UNPRUNED" | sort | tail -r | combine )
+  done
+  <<< "$LINES" cut -d' ' -f2-
+}
+
+function prune {
+  while read ; do
+    ENTRY=( $REPLY )
+    DIR=$( dirname "${ENTRY[1]}" )
+    SIZE="${ENTRY[2]}"
+    echo "$( <<< "${DIR}/" sed -e 's!//!/!' | grep -o / | tr -d $'\n' ) $DIR $SIZE"
+  done
+}
+
+readinput | process | cut -d' ' -f2 | awk '$1 <= 100000' | paste -s -d+ - | bc
