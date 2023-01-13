23e6e29a7fd32bac5c94059b0ea08f60d716899c
Matthew Maycock <ummaycoc@gmail.com>
2023-01-13 12:27:54 -0500

    Use printf.

--- day06/p2.sh
+++ day06/p2.sh
@@ -4,6 +4,6 @@ while read ; do
   current+=( $REPLY )
   index=$(( index + 1 ))
   [ "${#current[@]}" -gt 14 ] && current=( "${current[@]:1}" )
-  [ $( <<< "${current[@]}" tr ' ' $'\n' | sort -u | wc -l ) -eq 14 ] && break
+  [ $( printf '%s\n' "${current[@]}" | sort -u | wc -l ) -eq 14 ] && break
 done < <( < input grep -o . )
 echo $index
